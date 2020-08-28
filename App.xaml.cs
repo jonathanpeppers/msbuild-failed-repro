3617b2da87181745634cbeaffe20e4f782ff99cd
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-08-28 08:10:37 -0500

    Code pull over from unit test and fixed up

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace App1
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent ();
+
+			MainPage = new MainPage ();
+		}
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		public void Button_Clicked (object sender, EventArgs e)
+		{
+			Console.WriteLine ("Button was Clicked!");
+		}
+	}
+}
